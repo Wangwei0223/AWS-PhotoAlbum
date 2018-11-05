21f82cf7e8ab6eac76f2390c71527bd43d268981
Wangwei0223 <291978313@qq.com>
2018-11-04 20:31:49 -0500

    done Api GateWay with IAM

--- Assignment_1/app/utils/cognito.js
+++ Assignment_1/app/utils/cognito.js
@@ -2,6 +2,7 @@
 var AmazonCognitoIdentity = require('amazon-cognito-identity-js');
 import AWS from 'aws-sdk';
 import axios from 'axios';
+import sigV4Client from './sigV4Client';
 export function RegisterUser() {
     var poolData = {
         UserPoolId: 'us-west-2_d1PKrQeyR', // Your user pool id here
@@ -36,20 +37,20 @@ export function RegisterUser() {
     });
 }
 
-export function Confrim (code){
+export function Confrim(code) {
     var poolData = {
-        UserPoolId : 'us-west-2_d1PKrQeyR', // Your user pool id here
-        ClientId : '7i2i8gn6el0pb1vuqr4d3tduod' // Your client id here
+        UserPoolId: 'us-west-2_d1PKrQeyR', // Your user pool id here
+        ClientId: '7i2i8gn6el0pb1vuqr4d3tduod' // Your client id here
     };
 
     var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
     var userData = {
-        Username : '291978313@qq.com',
-        Pool : userPool
+        Username: '291978313@qq.com',
+        Pool: userPool
     };
 
     var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
-    cognitoUser.confirmRegistration(code, true, function(err, result) {
+    cognitoUser.confirmRegistration(code, true, function (err, result) {
         if (err) {
             alert(err.message || JSON.stringify(err));
             return;
@@ -58,21 +59,21 @@ export function Confrim (code){
     });
 }
 
-export function Auth(username, password){
+export function Auth(username, password) {
     var authenticationData = {
-        Username : username,
-        Password : password,
+        Username: username,
+        Password: password,
     };
     var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
 
     var poolData = {
-        UserPoolId : 'us-west-2_d1PKrQeyR', // Your user pool id here
-        ClientId : '7i2i8gn6el0pb1vuqr4d3tduod' // Your client id here
+        UserPoolId: 'us-west-2_d1PKrQeyR', // Your user pool id here
+        ClientId: '7i2i8gn6el0pb1vuqr4d3tduod' // Your client id here
     };
     var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
     var userData = {
-        Username : username,
-        Pool : userPool
+        Username: username,
+        Pool: userPool
     };
     var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
 
@@ -103,31 +104,31 @@ export function Auth(username, password){
             // });
         },
 
-        onFailure: function(err) {
+        onFailure: function (err) {
             alert(err.message || JSON.stringify(err));
         },
 
     });
 }
 
-export function Session(){
+export function Session() {
     var poolData = {
-        UserPoolId : 'us-west-2_d1PKrQeyR', // Your user pool id here
-        ClientId : '7i2i8gn6el0pb1vuqr4d3tduod' // Your client id here
+        UserPoolId: 'us-west-2_d1PKrQeyR', // Your user pool id here
+        ClientId: '7i2i8gn6el0pb1vuqr4d3tduod' // Your client id here
     };
     var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
     var cognitoUser = userPool.getCurrentUser();
     console.log('Current User is :');
     console.log(cognitoUser);
     if (cognitoUser != null) {
-        cognitoUser.getSession(function(err, session) {
+        cognitoUser.getSession(function (err, session) {
             if (err) {
                 alert(err.message || JSON.stringify(err));
                 return;
             }
             console.log('session validity: ' + session.isValid());
             //NOTE: getSession must be called to authenticate user before calling getUserAttributes
-            cognitoUser.getUserAttributes(function(err, attributes) {
+            cognitoUser.getUserAttributes(function (err, attributes) {
                 if (err) {
                     // Handle error
                 } else {
@@ -136,42 +137,59 @@ export function Session(){
             });
 
             AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-                IdentityPoolId : 'us-west-2:895c75a4-ca69-4ebf-b763-85f14357217f', // your identity pool id here
-                Logins : {
+                IdentityPoolId: 'us-west-2:895c75a4-ca69-4ebf-b763-85f14357217f', // your identity pool id here
+                Logins: {
                     // Change the key below according to the specific region your user pool is in.
-                    'cognito-idp.us-west-2.amazonaws.com/us-west-2_d1PKrQeyR' : session.getIdToken().getJwtToken()
+                    'cognito-idp.us-west-2.amazonaws.com/us-west-2_d1PKrQeyR': session.getIdToken().getJwtToken()
                 }
-            }, {region: 'us-west-2'});
+            }, { region: 'us-west-2' });
 
             var cred = AWS.config.credentials;
-        
+
             cred.refresh(function(err){
                 if (err) console.log(err);
                 else {
-                    console.log(cred);
-                    console.log(session.getIdToken().getJwtToken());
-                    axios.defaults.headers.common['Authorization'] = session.getIdToken().getJwtToken();
-                    axios.get('https://opmjcz5zk3.execute-api.us-west-2.amazonaws.com/test').then(function(data){
+                    let accessKey = cred.accessKeyId;
+                    let secretKey = cred.secretAccessKey;
+                    let sessionToken = cred.sessionToken;
+                    let region = 'us-west-2';
+                    console.log('accessKey: ' + accessKey);
+                    console.log('secretKey: ' + secretKey);
+                    console.log('sessionToken:' + sessionToken);
+                    let client = sigV4Client.newClient({
+                        // Your AWS temporary access key
+                        accessKey : accessKey,
+                        // Your AWS temporary secret key
+                        secretKey: secretKey,
+                        // Your AWS temporary session token
+                        sessionToken: sessionToken,
+                        // API Gateway region
+                        region:region ,
+                        // API Gateway URL
+                        endpoint: 'https://sr0igfw871.execute-api.us-west-2.amazonaws.com/demo'
+                      });
+                    let request = client.signRequest({
+                        method: 'GET',
+                        path: '',
+                        headers: {},
+                        queryParams: {},
+                        body:{}
+                      });
+                    
+                    console.log(request);
+
+                    // const results = fetch(request.url, {
+                    //     method: 'GET',
+                    //     headers: request.headers,
+                    // }).then(function(data){
+                    //     console.log(data);
+                    // });
+                    axios.defaults.headers = request.headers;
+                    axios.get('https://sr0igfw871.execute-api.us-west-2.amazonaws.com/demo').then(function(data){
                         console.log(data);
                     }).catch(function(err){
                         console.log(err);
                     });
-                    // var lambda = new AWS.Lambda({
-                    //     credentials: cred,
-                    //     region: 'us-west-2'
-                    // });
-                    // var params = {
-                    //     FunctionName: 'DemoSignUp',
-                    //     InvocationType: 'RequestResponse',
-                    //     Payload:''
-                    // };
-                    // lambda.invoke(params, function(err, data){
-                    //     if(err) console.log(err, err.stack);
-                    //     else {
-                    //         console.log(data);
-                    //     }
-                    // });
-
                 }
             });
 
