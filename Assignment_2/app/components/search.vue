054050186bfcf6617d46b16e405dc6dc227889ff
Wangwei0223 <291978313@qq.com>
2018-12-03 02:19:51 -0500

    add upload file & trans to base64

--- Assignment_2/app/components/search.vue
+++ Assignment_2/app/components/search.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <p>Search Image</p>
-    <input type="text" v-model="keyword"><span><button @click="send">Search</button></span>
+    <input type="text" v-model="keyword"><span><button @click="send">Search</button></span>  
+    <input type="file" name="" id="inputFile" @change="uploadIMG($event)" style="display:none"><span>{{filename}}</span>
+    <input type="button" value="Browse" onclick="document.getElementById('inputFile').click()">
   </div>
 </template>
 
@@ -9,9 +11,14 @@
 import { SendMessage } from "../utils/data";
 
 export default {
+  created(){
+    this.reader = new FileReader();
+  },
   data() {
     return {
-        keyword:''
+        keyword:'',
+        reader: '',
+        filename: ''
     };
   },
   methods:{
@@ -19,6 +26,24 @@ export default {
       SendMessage(this.keyword).then(data => {
         console.log(data);
       })
+    },
+    uploadIMG(e){
+      let files = e.target.files || e.dataTransfer.files;
+      if (!files.length) return;
+      console.log(files);
+      this.filename = files[0]['name'];
+      this.reader.readAsDataURL(files[0]);
+
+      this.reader.onload=function(e) {        //读取完毕后调用接口
+        let imgFile = e.target.result;
+        let obj={
+            id: "loginLogo",
+            configGroup: "logo",
+            configItem : "loginLogo",
+            itemValue : imgFile    
+        };
+        console.log(imgFile);
+      };   
     }
   }
 };
